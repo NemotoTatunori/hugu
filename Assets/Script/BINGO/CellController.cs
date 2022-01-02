31e53430989fd8ff2054717d118b836a48302c44
NemotoTatunori <toriakerasu@gmail.com>
2022-01-02 22:53:07 +0900

    空いた数字に違う色を付けた

--- Assets/Script/BINGO/CellController.cs
+++ Assets/Script/BINGO/CellController.cs
@@ -28,12 +28,37 @@ public class CellController : MonoBehaviour
     public void Hit()
     {
         m_open = true;
-        m_image.color = Color.gray;
+        StartCoroutine(Blinking());
     }
     /// <summary>フリーマス</summary>
     public void FreeCell()
     {
         m_text.text = "F";
         Hit();
+    }
+    /// <summary>点滅させるコルーチン</summary>
+    IEnumerator Blinking()
+    {
+        for (float i = 0; i < 1; i+= 0.01f)
+        {
+            m_image.color = new Color(i,i,0);
+            if (Input.GetMouseButtonUp(0))
+            {
+                m_image.color = Color.yellow;
+                break;
+            }
+            yield return null;
+        }
+        yield return null;
+        while (true)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                m_image.color = Color.gray;
+                break;
+            }
+            yield return null;
+        }
+
     }
 }
